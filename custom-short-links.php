221abbab85c5e4bcbaa0441bbc6bb2c76aefd79b
GitHub Action <>
2025-11-20 09:04:17 +0000

    build: update package file versions to 3.0.6

--- custom-short-links.php
+++ custom-short-links.php
@@ -4,7 +4,7 @@
  * Plugin Name:       Custom Short Links
  * Plugin URI:
  * Description:       Create short links for urls
- * Version: 3.0.5
+ * Version: 3.0.6
  * Author:            Kristoffer Svanmark
  * Author URI:
  * License:           MIT
